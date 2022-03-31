34adacb76e3cb79e67432ab8f1278774d4a16018
Joshua Ong <jong0054@student.monash.edu>
2022-04-01 09:56:47 +1100

    feat: part implemented betfair-racing-event-page.js

---
+++ racing-odds-scraper/src/scrapers/pages/racing-event-pages/betfair-racing-event-page.test.ts
@@ -0,0 +1,60 @@
+import { Console } from "console";
+import fs from "fs";
+import puppeteer, { Browser as PBrowser } from "puppeteer";
+import { Browser } from "../../browsers/browser";
+import { BetfairRacingEventPage } from "./betfair-racing-event-page";
+
+const bfRacingEventPageLogger = new Console({
+  stdout: fs.createWriteStream("./test-artifacts/betfair-racing-event-page-ut.txt")
+});
+
+const arbitraryBfRacingEventPageUrl = "https://www.betfair.com.au/exchange/plus/horse-racing/market/1.196917980";
+
+describe("BetfairRacingEventPage Unit Tests.", () => {
+  jest.setTimeout(20000);
+
+  let puppeteerBrowser: PBrowser;
+  let browser: Browser;
+
+  beforeAll(async () => {
+    puppeteerBrowser = await puppeteer.launch({ headless: true });
+    browser = new Browser(puppeteerBrowser);
+    
+    const bfRacingEventPageUrl = new URL(arbitraryBfRacingEventPageUrl);
+    const bfRacingEventPage = new BetfairRacingEventPage(bfRacingEventPageUrl);
+    const isAddPageSuccessful = await browser.addPage(bfRacingEventPage);
+    expect(isAddPageSuccessful).toBe(true);
+  });
+  
+  afterAll(async () => {
+    const bfSchedulePages = browser.pages.filter(p => p instanceof BetfairRacingEventPage);
+    expect(bfSchedulePages.length).toBe(1);
+    const [bfSchedulePage] = bfSchedulePages as BetfairRacingEventPage[];
+    
+    await bfSchedulePage!.page.setViewport({ width: 1920, height: 2500 });
+    await bfSchedulePage!.page.screenshot({ 
+      path: "./test-artifacts/betfair-racing-event-page-ut.jpeg", 
+      clip: { x: 0, y: 0, width: 1920, height: 2500 } 
+    }); // There is some capture-length/delay causing blur.
+    
+    await puppeteerBrowser.close();
+  })
+
+  test("BetfairRacingEventPage reads current contestants' names.", async () => {
+    const bfRacingEventPages = browser.pages.filter(p => p instanceof BetfairRacingEventPage);
+    expect(bfRacingEventPages.length).toBe(1);
+    const [bfRacingEventPage] = bfRacingEventPages as BetfairRacingEventPage[];
+    
+    const contestantsName = await bfRacingEventPage!.contestantsName();
+    bfRacingEventPageLogger.log(contestantsName);
+  });
+
+  test("BetfairRacingEventPage reads current horses' names.", async () => {
+    const bfRacingEventPages = browser.pages.filter(p => p instanceof BetfairRacingEventPage);
+    expect(bfRacingEventPages.length).toBe(1);
+    const [bfRacingEventPage] = bfRacingEventPages as BetfairRacingEventPage[];
+    
+    const contestantsToHorseMap = await bfRacingEventPage!.contestantsToHorseMap();
+    bfRacingEventPageLogger.log(contestantsToHorseMap);
+  });
+});
--- racing-odds-scraper/src/scrapers/pages/racing-event-pages/betfair-racing-event-page.ts
+++ racing-odds-scraper/src/scrapers/pages/racing-event-pages/betfair-racing-event-page.ts
@@ -1,13 +1,58 @@
 import { RacingEventPage } from "../racing-event-page";
 
-export abstract class BetfairRacingEventPage extends RacingEventPage {
+const betfairRacingEventPageConstants = {
+  html: {
+    classNames: {
+      jockeyName: "jockey-name",
+      horseName: "runner-name",
+    }
+  }
+}
+
+export class BetfairRacingEventPage extends RacingEventPage {
   constructor(url: URL) {
     super(url);
   }
 
-  contestantsName() {};
+  async contestantsName() {
+    if (!this.page) {
+      return console.log("Page is being searched w/o an underlying page object")
+    } else {
+      await this.page.waitForSelector(
+        `.${betfairRacingEventPageConstants.html.classNames.jockeyName}`
+      );
 
-  contestantsToHorseMap() {};
+      const contestantsName = await this.page.$$eval(
+        `.${betfairRacingEventPageConstants.html.classNames.jockeyName}`,
+        elements => elements.map(e => e.innerHTML)
+      );
+      return contestantsName;
+    }
+  };
 
-  contestantsToOddsMap() {};
+  async contestantsToHorseMap() {
+    if (!this.page) {
+      return console.log("Page is being searched w/o an underlying page object")
+    } else {
+      await this.page.waitForSelector(
+        `.${betfairRacingEventPageConstants.html.classNames.horseName}`
+      );
+
+      const horsesNamePlus = await this.page.$$eval(
+        `.${betfairRacingEventPageConstants.html.classNames.horseName}`,
+        elements => elements.map(e => e.innerHTML)
+      );
+      const horsesName = horsesNamePlus.map(cnp => cnp.slice(0, cnp.indexOf("<")));
+
+      const contestantsName = await this.contestantsName();
+
+      let contestantsToHorseMap = {};
+      contestantsName?.forEach((cn, i) => {
+        contestantsToHorseMap[cn] = horsesName[i]
+      });
+      return contestantsToHorseMap;
+    }
+  };
+
+  async contestantsToOddsMap() {};
 }
