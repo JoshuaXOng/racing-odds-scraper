979f5bfb3e197292eb20e844d3de9869aedcf4f4
JoshuaXOng <joshuaong2000@gmail.com>
2022-03-31 20:11:41 +1100

    Updated scraper constants.

--- racing-odds-scraper/src/scrapers/scraper-constants.ts
+++ racing-odds-scraper/src/scrapers/scraper-constants.ts
@@ -1,4 +1,5 @@
-// about:blank
+export const puppeteerInitialUrl = "about:blank";
+
 export const userAgentToHtmlHeaderMap = {
   nonHeadlessUA: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.0 Safari/537.36"
 }
