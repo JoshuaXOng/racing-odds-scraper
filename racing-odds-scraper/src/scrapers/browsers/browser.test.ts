a469df1f49cb96d3db895d3031f8a1b2ca3ad2fd
Joshua Ong <jong0054@student.monash.edu>
2022-03-31 16:13:24 +1100

    Commented out browser test bug.

--- racing-odds-scraper/src/scrapers/browsers/browser.test.ts
+++ racing-odds-scraper/src/scrapers/browsers/browser.test.ts
@@ -1,21 +1,22 @@
-import puppeteer from "puppeteer";
+import puppeteer, { Browser as PBrowser } from "puppeteer";
+import { bookiesToUrlsMap } from "../../constants";
 import { Browser } from "./browser";
 
 describe("Browser Unit Tests.", () => {
-//   let pBrowser: PBrowser;
+  let pBrowser: PBrowser;
 
-//   beforeAll(async () => {
-//     pBrowser = await launch({ headless: false });
-//   });
+  beforeAll(async () => {
+    pBrowser = await puppeteer.launch({ headless: false });
+  });
 
-//   afterAll(async () => {
-//     pBrowser.close();
-//   })
+  // // Causes error to be thown - reference error?
+  // afterAll(async () => {
+  //   pBrowser.close();
+  // })
   
-  test('Initialize Browser and open a Page.', async () => {
-    const pBrowser = await puppeteer.launch({ headless: true });
+  test("Initialize Browser and open Betfair's landing page.", () => {
     const browser = new Browser(pBrowser);
-    browser.addPage(new URL("https://www.youtube.com/"));
+    browser.addPage(new URL(bookiesToUrlsMap.betfair.index));
   });
 });
 
